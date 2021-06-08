045f74b0566448509626f865bbced5f8474412b1
Andyiev <ayevdokymenko@gmail.com>
2021-06-08 19:02:33 +0000

    Redirect After Form Submission. Redirect Short URLs

--- express_server.js
+++ express_server.js
@@ -36,10 +36,12 @@ app.get("/urls/new", (req, res) => {
 app.post("/urls", (req, res) => {
   const shortURL = generateRandomString();
   urlDatabase[shortURL] = req.body.longURL;
+  //console.log(urlDatabase);
   console.log(req.body);  // Log the POST request body to the console
-  console.log(urlDatabase[shortURL]);// Log the longURL
-  console.log(shortURL);//Log the shorURL
-  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+  //console.log(urlDatabase[shortURL]);// Log the longURL
+  //console.log(shortURL);//Log the shorURL
+  //res.send("Ok");         // Respond with 'Ok' (we will replace this)
+  res.redirect(`/urls/${shortURL}`);
 });
 
 
@@ -54,14 +56,20 @@ app.get("/urls/:shortURL", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
-app.get("/set", (req, res) => {
-  const a = 1;
-  res.send(`a = ${a}`);
- });
+app.get("/u/:shortURL", (req, res) => {
+  let temp = req.params.shortURL;
+  const longURL = urlDatabase[temp];
+  res.redirect(longURL);
+});
+
+// app.get("/set", (req, res) => {
+//   const a = 1;
+//   res.send(`a = ${a}`);
+//  });
  
- app.get("/fetch", (req, res) => {
-  res.send(`a = ${a}`);
- });
+//  app.get("/fetch", (req, res) => {
+//   res.send(`a = ${a}`);
+//  });
  
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
