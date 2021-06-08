9fdb8c2d92a8111057a5bd68aa0e3519dc1ba02a
Andyiev <ayevdokymenko@gmail.com>
2021-06-08 17:12:48 +0000

    Generate a Random ShortURL. Logging longURL and its shortURL

--- express_server.js
+++ express_server.js
@@ -10,9 +10,15 @@ const urlDatabase = {
   "S152tx": "http://www.tsn.ca"
 };
 
-const params = {
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
 
-}
+const generateRandomString = function() {
+  let randomString = Math.random().toString(36).substring(2,8);
+  //console.log("random", randomString);
+  return randomString;
+};
+
 
 app.get("/", (req, res) => {
   const templateVars = { greeting: 'Hello World!' };//new route handler for "hello world" and use res.render() to get this string formated (rendered from another file). 
@@ -22,6 +28,20 @@ app.get("/", (req, res) => {
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.post("/urls", (req, res) => {
+  const shortURL = generateRandomString();
+  urlDatabase[shortURL] = req.body.longURL;
+  console.log(req.body);  // Log the POST request body to the console
+  console.log(urlDatabase[shortURL]);// Log the longURL
+  console.log(shortURL);//Log the shorURL
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
+
 
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
